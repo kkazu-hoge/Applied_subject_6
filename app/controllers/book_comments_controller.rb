25dd9d738e365a53962c2014315c0e60cde033c4
kkazu-hoge <928kuni@gmail.com>
2022-04-01 07:46:20 +0000

    [finish]app_sub_6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,7 @@
 class BookCommentsController < ApplicationController
   include Common
   before_action :set_search_window
-  
+
   def create
     @book_comment = BookComment.new(book_comment_params)
     # binding.pry
@@ -9,19 +9,25 @@ class BookCommentsController < ApplicationController
     @book_comment[:user_id] = current_user.id
     # @book_commnet.user_id = current_user.id
     if @book_comment.save
-      redirect_to book_path(@book_comment.book_id)
+      #creat.js.erbでrenderする際に必要な変数
+      @book = Book.find(params[:book_id])
+      @book_comment = BookComment.new
+      # redirect_to book_path(@book_comment.book_id)
     else
       @book = Book.find(params[:book_id])
       @user = @book_comment.user
       @book_r = Book.new
-      render "books/show"
+      # render "books/show"
     end
   end
 
   def destroy
     @book_comment = BookComment.find(params[:id])
     @book_comment.destroy
-    redirect_to book_path(params[:book_id])
+    #destroy.js.erbでrenderする際に必要な変数
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    # redirect_to book_path(params[:book_id])
   end
 
   private
